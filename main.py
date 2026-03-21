3e6d1cf8a3a5664e790458f60ac4a81194dd5d76
isander94 <isaac.andersson0015@stud.hkr.se>
2026-03-21 22:25:31 +0100

    Added schemas and POST/GET /applications endpoints

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 import models
 from database import SessionLocal, engine
+import schemas
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -14,6 +15,33 @@ def get_db():
         yield db
     finally:
         db.close()
+
+# Routes
+# Post - Create a job application
+@app.post("/applications", response_model=schemas.ApplicationResponse)
+def create_application(
+    application: schemas.ApplicationCreate,
+    db: Session = Depends(get_db)
+):
+    # Create an SQLAlchemy object
+    db_application = models.Application(
+        title=application.title,
+        company=application.company,
+        status=application.status,
+        notes=application.notes
+    )
+    
+    db.add(db_application)
+    db.commit()
+    db.refresh(db_application)
+    
+    return db_application
+
+# Get - Fetch all job applications
+@app.get("/applications", response_model=list[schemas.ApplicationResponse])
+def get_applications(db: Session = Depends(get_db)):
+    applications = db.query(models.Application).all()
+    return applications
 
 # Default route
 @app.get("/")
---
+++ schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+# Schema used to create an application
+class ApplicationCreate(BaseModel):
+    title: str
+    company: str
+    status: str | None = "applied"
+    notes: str | None = None
+
+# Schema used to return data
+class ApplicationResponse(BaseModel):
+    id: int
+    title: str
+    company: str
+    status: str
+    notes: str | None
+    date_applied: datetime
+    
+    class Config:
+        from_attributes = True
